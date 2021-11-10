440c525b7d2eab1275ead8dbe47f2e924adb5b84
franckSDV <fgarciahu@gmail.com>
2021-11-10 19:47:44 -0300

    incluis clases con sqlite

---
+++ app/src/main/java/com/example/proyectobase/Clases_act.java
@@ -0,0 +1,14 @@
+package com.example.proyectobase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Clases_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clases);
+    }
+}
